12f0878bb9eaa83ce961c340ece0ca78cecc46d3
Sid Panjwani <sidhanthp@gmail.com>
2023-11-26 13:52:28 -0500

    fix video metadata

--- youtube.py
+++ youtube.py
@@ -1,9 +1,7 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 import urllib.parse as urlparse
 
-# from pytube import YouTube
-# import youtube_dl
-import pafy
+from pytube import YouTube
 import streamlit as st
 
 
@@ -14,72 +12,29 @@ def extract_video_id(url):
     return video_id
 
 
-# def get_video_metadata(url):
-#     """
-#     Fetches and returns the metadata of a YouTube video using pytube.
-#     """
-#     try:
-#         yt = YouTube(url)
-#         video_metadata = {
-#             "title": yt.title,
-#             "description": yt.description,
-#             "author": yt.author,
-#             "publish_date": yt.publish_date,
-#         }
-
-#         print(video_metadata)
-#         return video_metadata
-#     except Exception as e:
-#         print(f"An error occurred when trying to fetch the video metadata: {e}")
-#         return None
-
-
-# def get_video_metadata(url):
-#     """
-#     Fetches and returns the metadata of a YouTube video using youtube-dl.
-#     """
-#     ydl_opts = {
-#         "quiet": True,
-#         "no_warnings": True,
-#         "skip_download": True,
-#         "force_generic_extractor": True,
-#     }
-
-#     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#         try:
-#             video_info = ydl.extract_info(url, download=False)
-#             print(f"Video Info: ${video_info}")
-
-#             video_metadata = {
-#                 "title": video_info.get("title", None),
-#                 "description": video_info.get("description", None),
-#                 "author": video_info.get("uploader", None),
-#                 "publish_date": video_info.get("upload_date", None),
-#             }
-
-#             print(video_metadata)
-#             return video_metadata
-#         except Exception as e:
-#             print(f"An error occurred when trying to fetch the video metadata: {e}")
-#             return None
-
-
 def get_video_metadata(url):
     """
-    Fetches and returns the metadata of a YouTube video using Pafy.
+    Fetches and returns the metadata of a YouTube video using pytube.
     """
-    return
     try:
-        video = pafy.new(url)
-        print(video)
+        yt = YouTube(url)
+
+        # This is absolutely insane that the `description` is broken, but this GH issue had the answer: https://github.com/pytube/pytube/issues/1626
         video_metadata = {
-            "title": video.title,
-            # "description": video.description,
-            # "author": video.author,
-            # "publish_date": video.published,
+            "title": yt.title,
+            "description": yt.initial_data["engagementPanels"][1][
+                "engagementPanelSectionListRenderer"
+            ]["content"]["structuredDescriptionContentRenderer"]["items"][1][
+                "expandableVideoDescriptionBodyRenderer"
+            ][
+                "attributedDescriptionBodyText"
+            ][
+                "content"
+            ],
+            "author": yt.author,
+            "publish_date": yt.publish_date,
         }
 
-        print(video_metadata)
         return video_metadata
     except Exception as e:
         print(f"An error occurred when trying to fetch the video metadata: {e}")
@@ -108,7 +63,6 @@ def get_transcript(video_id):
             st.session_state.transcript = {}
         st.session_state.transcript[video_id] = transcript_with_timestamps
 
-        print(transcript_with_timestamps)
         return transcript_with_timestamps
     except Exception as e:
         print(f"An error occurred when trying to fetch the transcript: {e}")
